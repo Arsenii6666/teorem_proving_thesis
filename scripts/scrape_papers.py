106191ba343049fecaaa454718c7cbfb7ff09493
Arsenii6666 <92683180+Arsenii6666@users.noreply.github.com>
2025-01-08 20:29:30 +0200

    ruff format

--- scripts/scrape_papers.py
+++ scripts/scrape_papers.py
@@ -11,6 +11,7 @@ ARXIV_ID_PATH: Final = DATA_FOLDER / "arxiv_ids.csv"
 QUEUE_PATH: Final = DATA_FOLDER / "queue.txt"
 VISITED_PATH: Final = DATA_FOLDER / "visited.txt"
 
+
 @dataclass
 class Paper:
     arxiv_id: str | None
@@ -24,6 +25,7 @@ class Paper:
     venue: str | None
     year: int | None
 
+
 def get_citations_from_semantic_scholar(paper_id: str) -> list[Paper]:
     url = f"https://api.semanticscholar.org/v1/paper/{paper_id}"
     response = requests.get(url)
@@ -32,17 +34,18 @@ def get_citations_from_semantic_scholar(paper_id: str) -> list[Paper]:
         citations_data = data.get("citations", [])
         citations = [
             Paper(
-                arxiv_id=citation.get('arxivId'),
-                authors=citation.get('authors', []),
-                doi=citation.get('doi'),
-                intent=citation.get('intent', []),
-                is_influential=citation.get('isInfluential', False),
-                paper_id=citation['paperId'],
-                title=citation.get('title', ''),
-                url=citation.get('url'),
-                venue=citation.get('venue', ''),
-                year=citation.get('year')
-            ) for citation in citations_data
+                arxiv_id=citation.get("arxivId"),
+                authors=citation.get("authors", []),
+                doi=citation.get("doi"),
+                intent=citation.get("intent", []),
+                is_influential=citation.get("isInfluential", False),
+                paper_id=citation["paperId"],
+                title=citation.get("title", ""),
+                url=citation.get("url"),
+                venue=citation.get("venue", ""),
+                year=citation.get("year"),
+            )
+            for citation in citations_data
         ]
         return citations
     elif response.status_code == 429:
@@ -52,35 +55,42 @@ def get_citations_from_semantic_scholar(paper_id: str) -> list[Paper]:
         print(f"Error: {response.status_code} for Paper ID: {paper_id}")
         return []
 
+
 def save_queue(queue, filename=QUEUE_PATH):
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         for paper_id, depth in queue:
             file.write(f"{paper_id},{depth}\n")
 
+
 def load_queue(filename=QUEUE_PATH):
     if filename.exists():
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             queue = deque()
             for line in file:
-                paper_id, depth = line.strip().split(',')
+                paper_id, depth = line.strip().split(",")
                 queue.append((paper_id, int(depth)))
             return queue
     return deque()
+
 
 def save_visited(visited_papers, filename=VISITED_PATH):
     with filename.open("w") as file:
         for paper_id in visited_papers:
             file.write(f"{paper_id}\n")
 
+
 def load_visited(filename=VISITED_PATH):
     visited_papers = set()
     if filename.exists():
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             lines = file.readlines()
             visited_papers = set(line.strip() for line in lines)
     return visited_papers
 
-def find_all_citations(paper_id: str, delay, queue_file=QUEUE_PATH, visited_file=VISITED_PATH):
+
+def find_all_citations(
+    paper_id: str, delay, queue_file=QUEUE_PATH, visited_file=VISITED_PATH
+):
     papers = []
 
     queue = load_queue(queue_file)
@@ -115,15 +125,16 @@ def find_all_citations(paper_id: str, delay, queue_file=QUEUE_PATH, visited_file
     save_visited(visited_papers, visited_file)
     return papers
 
+
 def save_papers_to_csv(papers: list[Paper], filename=ARXIV_ID_PATH):
     df = pd.DataFrame(papers)
     df.to_csv(filename, mode="a")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print(DATA_FOLDER)
     main_paper_id = "87875a07976c26f82705de1fc70041169e5d652b"
     papers = find_all_citations(main_paper_id, delay=0.1)
     print(f"\nЗбереження інформації про {len(papers)} пейперів.")
     save_papers_to_csv(papers, ARXIV_ID_PATH)
     print("Дані збережено у файл 'data/arxiv_ids.csv'. Прогрес збережено.")
-
