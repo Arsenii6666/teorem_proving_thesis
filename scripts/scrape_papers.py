c0897be5147fed91334af7aaaf14137b698fe207
Arsenii6666 <92683180+Arsenii6666@users.noreply.github.com>
2025-01-08 20:24:53 +0200

    Update scrape_papers.py

--- scripts/scrape_papers.py
+++ scripts/scrape_papers.py
@@ -1,7 +1,7 @@
 import requests
 import time
 from collections import deque
-import csv
+import pandas as pd
 from dataclasses import dataclass
 from typing import Final
 from pathlib import Path
@@ -67,30 +67,24 @@ def load_queue(filename=QUEUE_PATH):
             return queue
     return deque()
 
-def save_visited(visited_papers, arxiv_ids, filename=VISITED_PATH):
-    with open(filename, 'w') as file:
+def save_visited(visited_papers, filename=VISITED_PATH):
+    with filename.open("w") as file:
         for paper_id in visited_papers:
             file.write(f"{paper_id}\n")
-        file.write("---\n")
-        for arxiv_id in arxiv_ids:
-            file.write(f"{arxiv_id}\n")
 
 def load_visited(filename=VISITED_PATH):
     visited_papers = set()
-    arxiv_ids = set()
     if filename.exists():
         with open(filename, 'r') as file:
             lines = file.readlines()
-            separator = lines.index('---\n')
-            visited_papers = set(line.strip() for line in lines[:separator])
-            arxiv_ids = set(line.strip() for line in lines[separator+1:])
-    return visited_papers, arxiv_ids
+            visited_papers = set(line.strip() for line in lines)
+    return visited_papers
 
 def find_all_citations(paper_id: str, delay, queue_file=QUEUE_PATH, visited_file=VISITED_PATH):
     papers = []
 
     queue = load_queue(queue_file)
-    visited_papers, arxiv_ids = load_visited(visited_file)
+    visited_papers = load_visited(visited_file)
 
     if not queue:
         queue.append((paper_id, 0))
@@ -104,11 +98,9 @@ def find_all_citations(paper_id: str, delay, queue_file=QUEUE_PATH, visited_file
             citations = get_citations_from_semantic_scholar(current_paper_id)
             if citations is None:
                 break
-            print(f"Depth: {depth}, Unique ArXiv IDs found: {len(arxiv_ids)}")
+            print(f"Depth: {depth}, Unique ArXiv IDs found: {len(visited_papers)}")
             for citation in citations:
                 citation_id = citation.paper_id
-                if citation.arxiv_id:
-                    arxiv_ids.add(citation.arxiv_id)
                 papers.append(citation)
                 if citation_id not in visited_papers:
                     queue.append((citation_id, depth + 1))
@@ -116,38 +108,20 @@ def find_all_citations(paper_id: str, delay, queue_file=QUEUE_PATH, visited_file
     except KeyboardInterrupt:
         print("\nProcess interrupted. Progress saved.")
         save_queue(queue, queue_file)
-        save_visited(visited_papers, arxiv_ids, visited_file)
+        save_visited(visited_papers, visited_file)
         return papers
 
     save_queue(queue, queue_file)
-    save_visited(visited_papers, arxiv_ids, visited_file)
+    save_visited(visited_papers, visited_file)
     return papers
 
 def save_papers_to_csv(papers: list[Paper], filename=ARXIV_ID_PATH):
-    if not filename.exists():
-        with open(filename, mode="w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(["arxiv_id", "authors", "doi", "intent", "is_influential", "paper_id", "title", "url", "venue", "year"])
-    with open(filename, mode="a", newline="") as file:
-        writer = csv.writer(file)
-        for paper in papers:
-            authors = ", ".join([author['name'] for author in paper.authors])
-            writer.writerow([paper.arxiv_id, authors, paper.doi, "; ".join(paper.intent), paper.is_influential, paper.paper_id, paper.title, paper.url, paper.venue, paper.year])
-
-def load_existing_papers(filename=ARXIV_ID_PATH):
-    existing_papers = set()
-    if filename.exists():
-        with open(filename, mode="r") as file:
-            reader = csv.reader(file)
-            next(reader)
-            for row in reader:
-                existing_papers.add(row[5])
-    return existing_papers
+    df = pd.DataFrame(papers)
+    df.to_csv(filename, mode="a")
 
 if __name__=="__main__":
     print(DATA_FOLDER)
     main_paper_id = "87875a07976c26f82705de1fc70041169e5d652b"
-    existing_papers = load_existing_papers(ARXIV_ID_PATH)
     papers = find_all_citations(main_paper_id, delay=0.1)
     print(f"\nЗбереження інформації про {len(papers)} пейперів.")
     save_papers_to_csv(papers, ARXIV_ID_PATH)
